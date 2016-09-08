9b756025c8c2028a0393ea2f76ce5dc441a5de84
Jonatan Frencia <frenciaj@gmail.com>
2016-09-08 13:06:31 -0400

    assetssss

--- config/application.rb
+++ config/application.rb
@@ -74,6 +74,8 @@ module Rims
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    config.serve_static_assets = true
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
