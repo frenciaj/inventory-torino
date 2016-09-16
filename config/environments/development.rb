cdf573e43ea7f73d584ff6d9842e17ec55efcdba
Jonatan Frencia <frenciaj@gmail.com>
2016-09-15 22:45:36 -0400

    add favicon on public folder

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,14 @@ Rims::Application.configure do
 #   authentication:       'plain',
 #   enable_starttls_auto: true  }
 config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
-
+# config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            ENV['GMAIL_USER'],
+  password:             ENV['GMAIL_PASS'],
+  authentication:       'plain',
+  enable_starttls_auto: true  }
 
 end
