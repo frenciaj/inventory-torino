86bf9ff825e01180cfe62fb989f000c72f92bfb9
Jonatan Frencia <frenciaj@gmail.com>
2016-09-17 09:59:43 -0400

    add 'cc' on mail controller

--- app/mailers/item_mailer.rb
+++ app/mailers/item_mailer.rb
@@ -3,6 +3,6 @@ class ItemMailer < ApplicationMailer
   def reporte_diario_email(user)
     @user = user
     @stocks = Stock.where("created_at >= ?", Date.today)
-mail(to: @user.email, subject: "Reporte de Stock del dia")
+mail(to: @user.email, cc: "herrajestorino@hotmail.com", subject: "Reporte de Stock del dia")
   end
 end
